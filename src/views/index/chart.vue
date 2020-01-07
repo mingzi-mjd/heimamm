bb79d8379ffa6d0c1d8eca0385e889e9d2e9a8c7
明子 <58070145+mingzi-mjd@users.noreply.github.com>
2020-01-07 18:02:20 +0800

    模块的简单布局

--- src/views/index/chart.vue
+++ src/views/index/chart.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>我是数据模块</div>
+  <!-- 统计模块 -->
+  <div class="chart-container">
+    <el-card class="box-card top"></el-card>
+    <el-card class="box-card bottom"></el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
 
-<style>
-
+<style lang='less'>
+.chart-container {
+  .top {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    margin-bottom: 20px;
+  }
+}
 </style>
--- src/views/index/enterprise.vue
+++ src/views/index/enterprise.vue
@@ -1,13 +1,238 @@
 <template>
-  <div>我是企业模块</div>
+  <!-- 企业模块 -->
+  <div class="enterprise-container">
+    <el-card class="box-card top">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline top-form">
+        <el-form-item class="fist-item" label="企业编号">
+          <el-input v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item class="two-item" label="企业名称">
+          <el-input v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item class="three-item" label="创建者">
+          <el-input v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item class="four-item" label="状态">
+          <el-select v-model="formInline.region" placeholder="请选择状态">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button>清除</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="dialogFormVisible = true">
+            <i class="el-icon-plus"></i> 新增企业
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card class="box-card bottom">
+      <el-table class="bottom-table" :data="tableData" style="width: 100%">
+        <el-table-column type="index" label="序号" width="80"></el-table-column>
+        <el-table-column label="企业编号">
+          <template slot-scope="scope">
+            <span>{{ scope.row.rid }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="企业名称">
+          <template slot-scope="scope">
+            <span>{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建者">
+          <template slot-scope="scope">
+            <span>{{ scope.row.username }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建日期">
+          <template slot-scope="scope">
+            <span>{{ scope.row.update_time }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <span v-if="scope.row.status==1">启用</span>
+            <span v-else>禁用</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button
+              type="text"
+              @click="handleDisabled(scope.row)"
+            >{{scope.row.status === 0 ? "启用" : "禁用"}}</el-button>
+            <el-button type="text" @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="block">
+        <el-pagination
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="pageSizes"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+    </el-card>
+    <el-dialog
+      title="新增企业"
+      :visible.sync="dialogFormVisible"
+      :show-close="false"
+      width="600px"
+      :center="true"
+      top="20vh"
+      custom-class="form-title"
+    >
+      <el-form :model="addForm" :rules="addRules" ref="addForm" class="formContent">
+        <el-form-item label="企业编号" prop="scienceid" class="addForm-one">
+          <el-input v-model="addForm.scienceid" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="企业名称" prop="sciencename" class="addForm-two">
+          <el-input v-model="addForm.sciencename" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="企业简称" prop="abbreviation">
+          <el-input v-model="addForm.abbreviation" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="企业介绍" prop="introduce">
+          <el-input v-model="addForm.introduce" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="企业备注" prop="remark">
+          <el-input v-model="addForm.remark" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      formInline: {
+        user: "",
+        region: ""
+      },
+      tableData: [],
+      currentPage: 1,
+      pageSizes: [10, 20, 30, 40, 50], // 每页的可选页数选项
+      pageSize: 10, // 每页显示条目个数
+      total: 0, // 总数据
 
-}
+      page: 0,
+      pageList: 0,
+
+      //  弹出的表单
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      addForm: {
+        scienceid: "", // 企业编号
+        sciencename: "", // 企业名称
+        abbreviation: "", // 企业简称
+        introduce: "", // 企业介绍
+        remark: "" // 企业备注
+      },
+      addRules: {
+        scienceid: [
+          { required: true, message: "企业编号不能为空", trigger: "blur" }
+        ],
+        sciencename: [
+          { required: true, message: "企业名称不能为空", trigger: "blur" },
+          {
+            min: 3,
+            max: 8,
+            message: "企业名称长度为3~8个字符",
+            trigger: "blur"
+          }
+        ]
+      },
+      flag: false
+    };
+  },
+  methods: {
+    onSubmit() {},
+    // 分页组件
+    handleSizeChange(pageList) {
+      window.console.log(`每页 ${pageList} 条`);
+      this.pageSize = pageList;
+      this.page = 1;
+      this.getList();
+    },
+    handleCurrentChange(page) {
+      window.console.log(`当前页: ${page}`);
+      this.page = page;
+      this.getList();
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang='less'>
+.enterprise-container {
+  .top {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    margin-bottom: 20px;
+    .el-form-item.fist-item {
+      margin-left: 20px;
+    }
+    .el-form-item {
+      margin-right: 11px;
+    }
+  }
+  .bottom {
+    .bottom-table {
+      margin-bottom: 30px;
+    }
+    .block {
+      text-align: center;
+    }
+  }
+  .el-form.formContent {
+    .el-form-item {
+      display: flex;
+      justify-content: space-between;
+      .el-input {
+        .el-input__inner {
+          width: 460px;
+        }
+      }
+    }
+    .addForm-one {
+      .el-form-item__label {
+        margin-left: -7px;
+      }
+    }
+    .addForm-two {
+      .el-form-item__label {
+        margin-left: -7px;
+      }
+    }
+  }
+  .el-dialog__header {
+    background: linear-gradient(
+      to right,
+      rgba(1, 198, 250, 1),
+      rgba(20, 147, 250, 1)
+    );
+    .el-dialog__title {
+      color: #fff;
+    }
+  }
+}
 </style>
